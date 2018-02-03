b39791b7324a141f8b0df3da5c24f8c4597c6f63
SachaG <358832+SachaG@users.noreply.github.com>
2018-02-03 10:53:55 +0900

    Refactor HoCs to show proper function name

--- packages/getting-started/lib/hocs/withMoviesCount.js
+++ packages/getting-started/lib/hocs/withMoviesCount.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const withMoviesCount = graphql(gql`
-  query MoviesCount{
-    MoviesCount
-  } 
-  `, {
-    alias: 'withMoviesCount',
-    options: {
-      pollInterval: 5
-    },
-    props(props) {
-      return {
-        loading: props.data.loading,
-        moviesCount: props.data.MoviesCount,
+const withMoviesCount = component => {
+  
+  return graphql(gql`
+    query MoviesCount{
+      MoviesCount
+    } 
+    `, {
+      alias: 'withMoviesCount',
+      options: {
+        pollInterval: 5
+      },
+      props(props) {
+        return {
+          loading: props.data.loading,
+          moviesCount: props.data.MoviesCount,
+        }
       }
     }
-  }
-);
+  )(component);
+};
 
 export default withMoviesCount;
--- packages/getting-started/lib/hocs/withMutationResolvers.js
+++ packages/getting-started/lib/hocs/withMutationResolvers.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const withMutationResolvers = graphql(gql`
-  query MutationResolvers{
-    __type(name:"Mutation") {
-      fields {
-        name
+const withMutationResolvers = component => {
+  
+  return graphql(gql`
+    query MutationResolvers{
+      __type(name:"Mutation") {
+        fields {
+          name
+        }
       }
     }
-  }
-  `, {
-    alias: 'withMutationResolvers',
-    props(props) {
-      return {
-        loading: props.data.loading,
-        mutations: props.data.__type,
+    `, {
+      alias: 'withMutationResolvers',
+      props(props) {
+        return {
+          loading: props.data.loading,
+          mutations: props.data.__type,
+        }
       }
     }
-  }
-);
+  )(component);
+};
 
 export default withMutationResolvers;
