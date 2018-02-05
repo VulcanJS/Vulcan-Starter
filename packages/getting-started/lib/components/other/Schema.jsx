1f5529a58c2ccba90a728906a97b4c373e261bd5
SachaG <358832+SachaG@users.noreply.github.com>
2018-02-05 18:42:01 +0900

    Use string for schema; small text tweaks

--- packages/getting-started/lib/components/other/Schema.jsx
+++ packages/getting-started/lib/components/other/Schema.jsx
@@ -3,11 +3,60 @@ import { registerComponent } from 'meteor/vulcan:core';
 import SyntaxHighlighter from 'react-syntax-highlighter/prism';
 import { okaidia } from 'react-syntax-highlighter/styles/prism';
 
-import schema from '../../modules/schema.js';
+// import schema from '../../modules/schema.js';
+
+const schema = `
+{
+
+  _id: {
+    type: String,
+    optional: true,
+    viewableBy: ['guests'],
+  },
+
+  createdAt: {
+    type: Date,
+    optional: true,
+    viewableBy: ['guests'],
+    onInsert: () => {
+      return new Date();
+    }
+  },
+
+  userId: {
+    type: String,
+    optional: true,
+    viewableBy: ['guests'],
+  },
+
+  name: {
+    label: 'Name',
+    type: String,
+    optional: true,
+    viewableBy: ['guests'],
+    insertableBy: ['members'],
+    editableBy: ['members'],
+    searchable: true,
+  },
+
+  review: {
+    label: 'Review',
+    type: String,
+    optional: true,
+    control: 'textarea',
+    viewableBy: ['guests'],
+    insertableBy: ['members'],
+    editableBy: ['members'],
+    searchable: true,
+  },
+
+}
+`
+console.log(schema)
 
 const Schema = () => (
   <div className="schema">
-    <SyntaxHighlighter language="javascript" style={okaidia}>{JSON.stringify(schema, null, 2)}</SyntaxHighlighter>
+    <SyntaxHighlighter language="javascript" style={okaidia}>{schema}</SyntaxHighlighter>
   </div>
 );
 
