f410ccb8449349351f16466dac026065e82f9e11
Discordius <panisnecis@gmail.com>
2018-06-04 17:21:34 -0700

    Modify RSS functionality

--- packages/example-forum/lib/modules/index.js
+++ packages/example-forum/lib/modules/index.js
@@ -10,4 +10,6 @@ export { Posts }  from './posts/index.js';
 export { Categories } from './categories/index.js';
 export { Comments }  from './comments/index.js';
 
+export { rssTermsToUrl }  from './rss_urls.js';
+
 import './notifications/index.js';
