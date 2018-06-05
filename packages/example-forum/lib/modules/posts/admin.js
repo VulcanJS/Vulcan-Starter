6fc0cc8770725135f3097ee48ac39408f4b19f4b
Discordius <panisnecis@gmail.com>
2018-06-04 17:21:49 -0700

    Deactivated admin posts column because it was causing weird bugs

--- packages/example-forum/lib/modules/posts/admin.js
+++ packages/example-forum/lib/modules/posts/admin.js
@@ -3,23 +3,24 @@
 Admin dashboard extension
 
 */
-
-import { extendFragment, addAdminColumn, addStrings } from 'meteor/vulcan:core';
-import AdminUsersPosts from '../../components/admin/AdminUsersPosts';
-
-extendFragment('UsersAdmin', `
-  posts(limit: 5){
-    ...PostsPage
-  }
-`);
-
-addAdminColumn({
-  name: 'posts',
-  order: 50,
-  component: AdminUsersPosts
-});
-
-
-addStrings('en', {
-  'admin.users.posts': 'Posts',
-});
+// LESSWRONG: DEACTIVATED THIS BECAUSE IT WAS CAUSING WEIRD BUGS
+// import { extendFragment, addAdminColumn, addStrings } from 'meteor/vulcan:core';
+// import AdminUsersPosts from '../../components/admin/AdminUsersPosts';
+//
+// extendFragment('UsersAdmin', `
+//   posts(limit: 5){
+//     ...PostsPage
+//   }
+//   IPs
+// `);
+//
+// addAdminColumn({
+//   name: 'posts',
+//   order: 50,
+//   component: AdminUsersPosts
+// });
+//
+//
+// addStrings('en', {
+//   'admin.users.posts': 'Posts',
+// });
