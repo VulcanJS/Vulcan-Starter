7360c581ad1d55a22d68ea1d18c78a2007eead71
Ryan Magoon <mr.magoon5@gmail.com>
2018-09-16 15:30:17 -0400

    update Vulcan core version

--- packages/example-interfaces/package.js
+++ packages/example-interfaces/package.js
@@ -1,27 +1,23 @@
 Package.describe({
-  name: 'example-interfaces',
-});
+  name: 'example-interfaces'
+})
 
-Package.onUse(function (api) {
-
+Package.onUse(function(api) {
   api.use([
-
     'promise',
 
     // vulcan core
-    'vulcan:core@1.12.4',
+    'vulcan:core@1.12.6',
 
     // vulcan packages
-    'vulcan:forms@1.12.4',
-    'vulcan:accounts@1.12.4',
-    'vulcan:ui-bootstrap@1.12.4',
-    
-  ]);
+    'vulcan:forms@1.12.6',
+    'vulcan:accounts@1.12.6',
+    'vulcan:ui-bootstrap@1.12.6'
+  ])
 
-  api.addFiles('lib/stylesheets/bootstrap.min.css');
-  api.addFiles('lib/stylesheets/custom.css');
+  api.addFiles('lib/stylesheets/bootstrap.min.css')
+  api.addFiles('lib/stylesheets/custom.css')
 
-  api.mainModule('lib/server/main.js', 'server');
-  api.mainModule('lib/client/main.js', 'client');
-
-});
+  api.mainModule('lib/server/main.js', 'server')
+  api.mainModule('lib/client/main.js', 'client')
+})
--- packages/example-reactions/package.js
+++ packages/example-reactions/package.js
@@ -1,27 +1,23 @@
 Package.describe({
-  name: 'example-reactions',
-});
+  name: 'example-reactions'
+})
 
-Package.onUse(function (api) {
-
+Package.onUse(function(api) {
   api.use([
-
     'promise',
 
     // vulcan core
-    'vulcan:core@1.12.4',
+    'vulcan:core@1.12.6',
 
     // vulcan packages
-    'vulcan:voting@1.12.4',
-    'vulcan:forms@1.12.4',
-    'vulcan:accounts@1.12.4',
-    'vulcan:ui-bootstrap@1.12.4',
-    
-  ]);
+    'vulcan:voting@1.12.6',
+    'vulcan:forms@1.12.6',
+    'vulcan:accounts@1.12.6',
+    'vulcan:ui-bootstrap@1.12.6'
+  ])
 
-  api.addFiles('lib/stylesheets/style.css');
-  
-  api.mainModule('lib/server/main.js', 'server');
-  api.mainModule('lib/client/main.js', 'client');
+  api.addFiles('lib/stylesheets/style.css')
 
-});
+  api.mainModule('lib/server/main.js', 'server')
+  api.mainModule('lib/client/main.js', 'client')
+})
