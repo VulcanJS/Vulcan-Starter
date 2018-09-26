bcf56aaba472fc752bbe6fcef8ed29c54574ea2d
Eric Burel <e.burel.contact@gmail.com>
2018-09-26 11:08:35 +0200

    updated router components to RR4
    Use react-router-dom to import component. IndexLink becomes NavLink with exact props, Link with style
    (like activeClassName) become NavLink

--- packages/example-forum/lib/components/posts/PostsCommenters.jsx
+++ packages/example-forum/lib/components/posts/PostsCommenters.jsx
@@ -1,18 +1,22 @@
 import { Components, registerComponent } from 'meteor/vulcan:core';
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Posts } from '../../modules/posts/index.js';
 
-const PostsCommenters = ({post}) => {
+const PostsCommenters = ({ post }) => {
   return (
     <div className="posts-commenters">
       <div className="posts-commenters-avatars">
-        {_.take(post.commenters, 4).map(user => user && <Components.UsersAvatar key={user._id} user={user}/>)}
+        {_.take(post.commenters, 4).map(
+          user => user && <Components.UsersAvatar key={user._id} user={user} />
+        )}
       </div>
       <div className="posts-commenters-discuss">
         <Link to={Posts.getPageUrl(post)}>
           <Components.Icon name="comment" />
-          <span className="posts-commenters-comments-count">{post.commentCount}</span>
+          <span className="posts-commenters-comments-count">
+            {post.commentCount}
+          </span>
           <span className="sr-only">Comments</span>
         </Link>
       </div>
@@ -20,6 +24,6 @@ const PostsCommenters = ({post}) => {
   );
 };
 
-PostsCommenters.displayName = "PostsCommenters";
+PostsCommenters.displayName = 'PostsCommenters';
 
 registerComponent({ name: 'PostsCommenters', component: PostsCommenters });
