269e5ae5d99cd2fabdf25ee4c129cdc95eac11c2
SachaG <358832+SachaG@users.noreply.github.com>
2019-02-16 17:39:32 +0900

    Update bootstrap and font-awesome

--- packages/example-permissions/lib/components/common/Layout.jsx
+++ packages/example-permissions/lib/components/common/Layout.jsx
@@ -13,17 +13,14 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { replaceComponent, Components } from 'meteor/vulcan:core';
 
-// note: modal popups won't work with anything above bootstrap alpha.5. 
-// see https://github.com/twbs/bootstrap/issues/21876#issuecomment-276181539
-
 const Layout = ({children}) =>
 
   <div className="wrapper" id="wrapper">
 
     <Helmet>
       <title>Vulcanstagram</title>
-      <link name="bootstrap" rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"/>
-      <link name="font-awesome" rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+      <link name="bootstrap" rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"/>
+      <link name="font-awesome" rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     </Helmet>
 
     <Components.Header/>
