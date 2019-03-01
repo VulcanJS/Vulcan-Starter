1d794955efe6bcb256c1c8811d62644aa56e867e
eric-burel <eb@lebrun-burel.com>
2019-03-01 10:21:33 +0100

    better finding of Vulcan packages path

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,11 +4,38 @@ Modify
 
 */
 
+/**
+ * Smart function to find Vulcan packages
+ * 
+ * You can either provide a path to Vulcan as VULCAN_DIR env
+ * or set the METEOR_PACKAGE_DIR variable
+ */
+const findPathToVulcanPackages = () => {
+  // look for VULCAN_DIR env variable
+  if (process.env.VULCAN_DIR) return `${process.env.VULCAN_DIR}/packages`
+  // look for METEOR_PACKAGE_DIRS variable
+  const rawPackageDirs = process.env.METEOR_PACKAGE_DIRS
+  if (rawPackageDirs) {
+    const dirs = rawPackageDirs.split(':')
+    // Vulcan dir should be '/some-folder/Vulcan/packages'
+    const vulcanPackagesDir = dirs.find((dir) => !!dir.match(/\/Vulcan\//))
+    if (vulcanPackagesDir) {
+      return vulcanPackagesDir
+    }
+    console.log(`
+      Please either set the VULCAN_DIR variable to your Vulcan folder or
+      set METEOR_PACKAGE_DIRS to your <Vulcan>/packages folder.
+      Fallback to default value: '../../Vulcan'.`
+    )
+  }
+  // default value
+  return '../../Vulcan/packages'
+}
 // path to your Vulcan repo (see 2-repo install in docs)
-const pathToVulcan = '../../Vulcan';
+const pathToVulcanPackages = findPathToVulcanPackages();
 
 // path to your Vulcan UI library package
-const pathToUILibrary = `${pathToVulcan}/packages/vulcan-ui-bootstrap`; 
+const pathToUILibrary = `${pathToVulcanPackages}/vulcan-ui-bootstrap`;
 
 /*
 
@@ -24,15 +51,15 @@ module.exports = storybookBaseConfig => {
       ...storybookBaseConfig.resolve.alias,
 
       // Components
-      CoreComponentsLoader: path.resolve(__dirname, `${pathToVulcan}/packages/vulcan-core/lib/modules/components.js`),
+      CoreComponentsLoader: path.resolve(__dirname, `${pathToVulcanPackages}/vulcan-core/lib/modules/components.js`),
       UIComponentsLoader: path.resolve(__dirname, `${pathToUILibrary}/lib/modules/components.js`),
       UILibrary: path.resolve(__dirname, pathToUILibrary),
-      
+
       // Locales
-      EnUS: path.resolve(__dirname, `${pathToVulcan}/packages/vulcan-i18n-en-us/lib/en_US.js`),
-      EsES: path.resolve(__dirname, `${pathToVulcan}/packages/vulcan-i18n-es-es/lib/es_ES.js`),
-      FrFR: path.resolve(__dirname, `${pathToVulcan}/packages/vulcan-i18n-fr-fr/lib/fr_FR.js`),
-      
+      EnUS: path.resolve(__dirname, `${pathToVulcanPackages}/vulcan-i18n-en-us/lib/en_US.js`),
+      EsES: path.resolve(__dirname, `${pathToVulcanPackages}/vulcan-i18n-es-es/lib/es_ES.js`),
+      FrFR: path.resolve(__dirname, `${pathToVulcanPackages}/vulcan-i18n-fr-fr/lib/fr_FR.js`),
+
       // Vulcan Packages
       'meteor/vulcan:lib': path.resolve(__dirname, './helpers.js'),
       'meteor/vulcan:core': path.resolve(__dirname, './helpers.js'),
