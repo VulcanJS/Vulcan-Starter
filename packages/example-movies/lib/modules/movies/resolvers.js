c8f1a5bb75d974e2fe20baf9efc8ca9c516c41f8
Apollinaire Lecocq <Apollinaire@users.noreply.github.com>
2019-08-02 14:08:25 +0200

    Fix missing const in movies resolver
    I don't even know why I wrote it like that in the first place :man_shrugging:

--- packages/example-movies/lib/modules/movies/resolvers.js
+++ packages/example-movies/lib/modules/movies/resolvers.js
@@ -15,9 +15,9 @@ const resolvers = {
     async resolver(root, args, context) {
       const { input: {terms = {}} = {terms: {}} } = args;
       let { selector, options } = await context.Movies.getParameters(terms, {}, context.currentUser);
-      movies = await context.Movies.find(selector, options);
-      moviesContent = movies.fetch();
-      moviesCount = movies.count();
+      const movies = await context.Movies.find(selector, options);
+      const moviesContent = movies.fetch();
+      const moviesCount = movies.count();
       return { results: moviesContent, totalCount: moviesCount };
     },
   },
