a514dd8a0808789ca091ba0d2733a788fea2a9b3
eric-burel <eb@lebrun-burel.com>
2020-02-18 17:58:17 +0100

    bump 1.14.1

--- packages/example-forms/package.js
+++ packages/example-forms/package.js
@@ -6,13 +6,13 @@ Package.onUse(function (api) {
 
   api.use([
     // vulcan core
-    'vulcan:core@=1.14.0',
+    'vulcan:core@=1.14.1',
 
     // vulcan packages
-    'vulcan:forms@=1.14.0',
-    'vulcan:accounts@=1.14.0',
-    'vulcan:forms-upload@=1.14.0',
-    'vulcan:ui-bootstrap@=1.14.0',
+    'vulcan:forms@=1.14.1',
+    'vulcan:accounts@=1.14.1',
+    'vulcan:forms-upload@=1.14.1',
+    'vulcan:ui-bootstrap@=1.14.1',
 
     // third-party packages
     'fourseven:scss@4.5.0',
