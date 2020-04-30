c8638f4a477e21061180a88766d17cef819ce9fa
eric-burel <eb@lebrun-burel.com>
2020-04-30 08:42:56 +0200

    minimal changes to update to meteor 10

--- packages/_example-membership/package.js
+++ packages/_example-membership/package.js
@@ -1,35 +1,29 @@
 Package.describe({
-  name: 'example-membership',
+  name: "example-membership",
 });
 
 Package.onUse(function (api) {
-
   api.use([
-
-    'promise',
+    "promise",
 
     // vulcan core
-    'vulcan:core@=1.14.1',
+    "vulcan:core@=1.14.1",
 
     // vulcan packages
-    'vulcan:forms@=1.14.1',
-    'vulcan:accounts@=1.14.1',
-    'vulcan:forms-upload@=1.14.1',
-    'vulcan:payments@=1.14.1',
-    'vulcan:ui-bootstrap@=1.14.1',
+    "vulcan:forms@=1.14.1",
+    "vulcan:accounts@=1.14.1",
+    "vulcan:forms-upload@=1.14.1",
+    "vulcan:payments@=1.14.1",
+    "vulcan:ui-bootstrap@=1.14.1",
 
     // third-party packages
-    'fourseven:scss@4.5.0',
-
+    "fourseven:scss@4.12.0",
   ]);
 
-  api.addFiles('lib/stylesheets/style.scss');
+  api.addFiles("lib/stylesheets/style.scss");
 
-  api.addAssets([
-    'lib/static/vulcanstagram.png'
-  ], ['client']);
+  api.addAssets(["lib/static/vulcanstagram.png"], ["client"]);
 
-  api.mainModule('lib/server/main.js', 'server');
-  api.mainModule('lib/client/main.js', 'client');
-
+  api.mainModule("lib/server/main.js", "server");
+  api.mainModule("lib/client/main.js", "client");
 });
--- packages/getting-started/package.js
+++ packages/getting-started/package.js
@@ -1,27 +1,23 @@
 Package.describe({
-  name: 'getting-started',
+  name: "getting-started",
 });
 
 Package.onUse(function (api) {
-
   api.use([
-
     // SASS/SCSS support
-    'fourseven:scss@4.5.0',
+    "fourseven:scss@4.12.0",
 
     // vulcan core
-    'vulcan:core@=1.14.1',
+    "vulcan:core@=1.14.1",
 
     // vulcan packages
-    'vulcan:forms@=1.14.1',
-    'vulcan:accounts@=1.14.1',
-    'vulcan:ui-bootstrap@=1.14.1',
-
+    "vulcan:forms@=1.14.1",
+    "vulcan:accounts@=1.14.1",
+    "vulcan:ui-bootstrap@=1.14.1",
   ]);
 
-  api.addFiles('lib/stylesheets/style.scss');
+  api.addFiles("lib/stylesheets/style.scss");
 
-  api.mainModule('lib/server/main.js', 'server');
-  api.mainModule('lib/client/main.js', 'client');
-
+  api.mainModule("lib/server/main.js", "server");
+  api.mainModule("lib/client/main.js", "client");
 });
