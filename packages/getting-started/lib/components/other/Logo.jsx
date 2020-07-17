d69265f6949de2ec89b49ad762bb0e09c28b3e52
SachaG <358832+SachaG@users.noreply.github.com>
2020-07-17 18:32:33 +0900

    Logo tweaks

--- packages/getting-started/lib/components/other/Logo.jsx
+++ packages/getting-started/lib/components/other/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = () => (
   <div className="logo">
-    <img src="/vulcan-logo-negative.svg" />
+    <Link to="/">
+      <img src="/vulcan-logo-negative.svg" />
+    </Link>
   </div>
 );
 
